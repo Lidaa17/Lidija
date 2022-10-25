dc69b5f2571f53e538708b0b12a71fcba420c1cb
Lidija Djurdjic <lidija.dj17@gmail.com>
2022-10-25 13:40:17 +0200

    Dodat gitignore fajl

---
+++ ConsoleExample/Model/Vehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelConsoleExample.Model
+{
+    class Vehicle
+    {
+        public int NumberOfWheels { get; set; }
+        public string Name { get; set; }
+
+        public Vehicle(int numberOfWheels, string name)
+        {
+            NumberOfWheels = numberOfWheels;
+            Name = name;
+        }
+
+        public Vehicle() { }
+
+        public override string ToString()
+        {
+            return String.Format("| Name: {0,20} | NumberOfWheels: {1,5} |", Name, NumberOfWheels);
+        }
+
+    }
+}
---
+++ ConsoleExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using ModelConsoleExample.Model;
+using System;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle vehicle = InputVehicle();
+            Console.WriteLine(vehicle);
+
+            Console.ReadKey();
+        }
+
+        static Vehicle InputVehicle()
+        {
+            Vehicle vehicle = new Vehicle();
+
+            Console.WriteLine("Enter vehicle name: ");
+            String name = Console.ReadLine();
+            vehicle.Name = name;
+
+            Console.WriteLine("Enter No. of wheels: ");
+            int wheels = Convert.ToInt32(Console.ReadLine());
+            vehicle.NumberOfWheels = wheels;
+
+            return vehicle;
+        }
+    }
+}
